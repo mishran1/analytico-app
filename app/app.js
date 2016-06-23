8568906bd9a18d7cc0b5e17ac85c131f4a9651dd
Nishant Mishra <nishantmishra0@gmail.com>
2016-06-23 14:41:26 -0400

    First commit

---
+++ app/app.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ui.router'])
+        .config(config)
+        .run(run);
+
+    function config($stateProvider, $urlRouterProvider) {
+        // default route
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'home/index.html',
+                controller: 'Home.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'home' }
+            })
+            .state('account', {
+                url: '/account',
+                templateUrl: 'account/index.html',
+                controller: 'Account.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'account' }
+            })
+            .state('analytics', {
+                url: '/analytics',
+                templateUrl: 'analytics/index.html',
+                controller: 'analytics.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'analytics' }
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'about/index.html',
+                controller: 'about.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'about' }
+            })
+            .state('logout', {
+                url: '/logout',
+                templateUrl: '',
+                controller: 'logout.LogoutController',
+                controllerAs: 'vm',
+                data: { activeTab: 'logout' }
+            });
+    }
+
+    function run($http, $rootScope, $window) {
+        // add JWT token as default auth header
+        $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
+
+        // update active tab on state change
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            $rootScope.activeTab = toState.data.activeTab;
+        });
+    }
+
+    // manually bootstrap angular after the JWT token is retrieved from the server
+    $(function () {
+        // get JWT token from server
+        $.get('/app/token', function (token) {
+            window.jwtToken = token;
+
+            angular.bootstrap(document, ['app']);
+        });
+    });
+})();
