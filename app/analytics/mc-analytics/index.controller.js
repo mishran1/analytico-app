186e725d71fa4a8c0a70ae44e81ce2d7031d9245
tejas1337 <tejas@maisonmrkt.com>
2016-07-22 16:48:16 -0400

    Added HighChartsJS for MailChimp

--- app/analytics/mc-analytics/index.controller.js
+++ app/analytics/mc-analytics/index.controller.js
@@ -5,23 +5,73 @@
         .module('app')
         .controller('MailChimp.IndexController', Controller)
 
-    function Controller(UserService, $rootScope) {
+    function Controller(UserService, $rootScope, $scope) {
     	$rootScope.flag = 1;
-      	console.log("You are accessing the MailChimp Analytics page");
+    	console.log("You are accessing the MailChimp Analytics page");
 
-      	var vm = this;
+    	var vm = this;
+    	vm.user = null;
 
-      	vm.user = null;
+  		UserService.GetCurrent().then(function (user) {
+  			UserService.GetMC().then(function(data) {
+          console.log(data);
 
-      	initController();
+          Highcharts.chart('container', {
 
-      	function initController() {
-      		UserService.GetCurrent().then(function (user) {
-      			UserService.GetMC().then(function(data) {
-      				console.log(data);
-      			})
-      		});
-      	}
+              chart: {
+                type: 'column'
+              },
+
+              title: {
+                  text: 'Subscribers for mailing Lists'
+              },
+
+              subtitle: {
+                  text: 'Click the columns to view monthly subscribers'
+              },
+
+              xAxis: {
+                  type: 'category'
+              },
+
+              yAxis: {
+                  title: {
+                      text: 'Users'
+                  }
+
+              },
+
+              legend: {
+                  enabled: false
+              },
+
+              plotOptions: {
+                  series: {
+                      borderWidth: 0,
+                      dataLabels: {
+                          enabled: true,
+                          format: '{point.y}'
+                      }
+                  }
+              },
+
+              tooltip: {
+                  headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                  pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> users added to Total Subscribers<br/>'
+              },
+
+              series: [{
+                  name: 'Lists',
+                  colorByPoint: true,
+                  data: data.obj1
+              }],
+
+              drilldown: {
+                  series: data.obj2
+              }
+          });
+  			})
+  		});
     }
 
 })();
--- controllers/api/MC.controller.js
+++ controllers/api/MC.controller.js
@@ -9,58 +9,88 @@ router.get('/MC', getMC);
 module.exports = router;
 
 function getMC(req, res) {
-    //Store 
-    var cid = [];
-    var MCdata = [];
-    
+    //Variables to store data requested from MailChimpAPI
+    var lid = [];               //List IDs
+    var TopLevel = [];          //Top Level Data for drill down chart
+    var DetailedData = [];          //Detailed Data
     userService.getById(req.user.sub)
         .then(function (user) {
             if (user) {
+                //Initialize MailChimp Object
                 try {
                     var mailchimp = new Mailchimp(user.apiKey);
                 } catch (error) {
                     console.log(error.message);
                 }
                 
-                var get_cid = new Promise(function(resolve, reject) {
+                //Promise to retrieve list ids from the API object
+                var get_ids = new Promise(function(resolve, reject) {
                     var data = mailchimp.request({
                     method : 'get',
-                    path : 'reports'
+                    path : 'lists'
                     }, function (err,result) {
                         if (err) {
-                            resolve("It broke");
+                            reject(err);
                         }
-                        for (var i = 0; i < result.reports.length; i++) {
-                          cid.push(result.reports[i].id);
+
+                        for (var i = 0; i < result.lists.length; i++) {
+                          lid.push({
+                            id: result.lists[i].id,
+                            title: result.lists[i].name
+                            });
                         }
-                        resolve(cid);
+                        resolve(lid);
                     })
                 })
 
-                get_cid.then(function(result) {
-                    var counter = 0;
-                    var limit = result.length;
+                get_ids.then(function(result) {
+                    var counter = 0;    //Count number of list ids processed
+                    var limit = result.length;  //Total number of list ids
+
+                    //Loop through list ids and retrieve growth history for each
                     for (var i = 0; i < result.length; i++) {
-                        var get_loc = new Promise(function(resolve, reject) {
-                            var data1 = mailchimp.request({
+                        var get_history = new Promise(function(resolve, reject) {
+                            var title = result[i].title;
+                            var data = mailchimp.request({
                                 method: 'get',
-                                path : '/reports/{campaign_id}/locations',
+                                path : '/lists/{list_id}/growth-history',
                                 path_params: {
-                                    campaign_id : result[i]
+                                    list_id : result[i].id
                                 }
                             }, function (err, result) {
                                 if (err) {
-                                    resolve("It Broke");
+                                    reject(err);
                                 }
-                                resolve(result);
+                                var subs = 0; //Variable for total Subscriptions
+                                var monthly = [];
+
+                                //Loop through each month
+                                result.history.forEach(function (row, i) {
+                                    subs = subs + row.imports + row.optins;
+                                    monthly.push([row.month, row.imports + row.optins]);
+                                });
+
+                                resolve({
+                                    top: {
+                                    name: title,
+                                    y: subs,
+                                    drilldown: title
+                                    },
+                                    down: {
+                                    id: title,
+                                    name: title,
+                                    data: monthly
+                                    }
+                                });
                             })
                         })
 
-                        get_loc.then(function(result) {
-                            MCdata.push(result.locations);
+                        get_history.then(function(result) {
                             counter = counter+1;
+                            TopLevel.push(result.top);
+                            DetailedData.push(result.down);
                             if (counter == limit) {
-                                res.send(MCdata);
+                                res.send({ obj1: TopLevel, obj2: DetailedData});
                             }
                         }, function(err) {
                             console.log(err);
