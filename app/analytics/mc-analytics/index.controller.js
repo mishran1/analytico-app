c6d95e3e909c8335945332ffb67b1b1a51930c3e
tejas1337 <tejas@maisonmrkt.com>
2016-07-26 17:18:59 -0400

    Added MailChimp Batch request functionality

--- app/analytics/mc-analytics/index.controller.js
+++ app/analytics/mc-analytics/index.controller.js
@@ -6,14 +6,14 @@
         .controller('MailChimp.IndexController', Controller)
 
     function Controller(UserService, $rootScope, $scope) {
-    	$rootScope.flag = 1;
-    	console.log("You are accessing the MailChimp Analytics page");
+      $rootScope.flag = 1;
+      console.log("You are accessing the MailChimp Analytics page");
 
-    	var vm = this;
-    	vm.user = null;
+      var vm = this;
+      vm.user = null;
 
-  		UserService.GetCurrent().then(function (user) {
-  			UserService.GetMC().then(function(data) {
+      UserService.GetCurrent().then(function (user) {
+        UserService.GetMC().then(function(data) {
           console.log(data);
 
           Highcharts.chart('container', {
@@ -63,15 +63,78 @@
               series: [{
                   name: 'Lists',
                   colorByPoint: true,
-                  data: data.obj1
+                  data: data[0].obj1
               }],
 
               drilldown: {
-                  series: data.obj2
+                  series: data[1].obj2
               }
           });
-  			})
-  		});
+
+          var click_rates = [];
+          var open_rates = [];
+          var names = [];
+
+          data[2].obj3.forEach(function(row, i) {
+            click_rates.push(row.click_rate);
+            open_rates.push(row.open_rate);
+            names.push(row.name);
+          });
+
+          Highcharts.chart('container1', {
+            title: {
+              text: 'Engagement Analysis'
+            },
+            xAxis: {
+              categories: names
+            },
+            labels: {
+                items: [{
+                    html: 'Total fruit consumption',
+                    style: {
+                        left: '50px',
+                        top: '18px',
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+                    }
+                }]
+            },
+            credits: {
+              enabled: false
+            },
+            series: [{
+                type: 'column',
+                name: 'Click Rate',
+                data: click_rates
+            }, {
+                type: 'column',
+                name: 'Open Rate',
+                data: open_rates
+            }, {
+                type: 'pie',
+                name: 'Total consumption',
+                data: [{
+                    name: 'Jane',
+                    y: 13,
+                    color: Highcharts.getOptions().colors[3] // Jane's color
+                }, {
+                    name: 'John',
+                    y: 23,
+                    color: Highcharts.getOptions().colors[1] // John's color
+                }, {
+                    name: 'Joe',
+                    y: 19,
+                    color: Highcharts.getOptions().colors[2] // Joe's color
+                }],
+                center: [20, 20],
+                size: 100,
+                showInLegend: false,
+                dataLabels: {
+                    enabled: false
+                }
+            }]
+          });
+        })
+      });
     }
 
 })();
--- controllers/api/MC.controller.js
+++ controllers/api/MC.controller.js
@@ -11,8 +11,10 @@ module.exports = router;
 function getMC(req, res) {
     //Variables to store data requested from MailChimpAPI
     var lid = [];               //List IDs
-    var TopLevel = [];          //Top Level Data for drill down chart
-    var DetailedData = [];          //Detailed Data
+    var rid = [];               //List IDs
+    var map = {};
+    var MCdata = [];
+
     userService.getById(req.user.sub)
         .then(function (user) {
             if (user) {
@@ -26,76 +28,140 @@ function getMC(req, res) {
                 //Promise to retrieve list ids from the API object
                 var get_ids = new Promise(function(resolve, reject) {
                     var data = mailchimp.request({
-                    method : 'get',
-                    path : 'lists'
+                        method : 'get',
+                        path : 'lists'
+                    }, function (err,result) {
+                        if (err) {
+                            reject(err);
+                        }
+                        
+                        for (var i = 0; i < result.lists.length; i++) {
+                            map[result.lists[i].id] = result.lists[i].name;
+                        }
+                    })
+
+                    var data1 = mailchimp.request({
+                        method : 'get',
+                        path : 'reports'
                     }, function (err,result) {
                         if (err) {
                             reject(err);
                         }
 
-                        for (var i = 0; i < result.lists.length; i++) {
-                          lid.push({
-                            id: result.lists[i].id,
-                            title: result.lists[i].name
+                        for (var i = 0; i < result.reports.length; i++) {
+                            rid.push({
+                                id: result.reports[i].id,
+                                title: result.reports[i].campaign_title
                             });
                         }
-                        resolve(lid);
                     })
+
+                    setTimeout(function(){ resolve({ lists: map, reports: rid }) }, 2000);
                 })
 
-                get_ids.then(function(result) {
-                    var counter = 0;    //Count number of list ids processed
-                    var limit = result.length;  //Total number of list ids
 
+                get_ids.then(function(result) {
                     //Loop through list ids and retrieve growth history for each
-                    for (var i = 0; i < result.length; i++) {
-                        var get_history = new Promise(function(resolve, reject) {
-                            var title = result[i].title;
-                            var data = mailchimp.request({
-                                method: 'get',
-                                path : '/lists/{list_id}/growth-history',
-                                path_params: {
-                                    list_id : result[i].id
+                    var hist_req = [];
+                    var act_req = [];
+                    for (var i in result.lists) {
+                        hist_req.push({
+                            method : 'get',
+                            path : '/lists/{list_id}/growth-history',
+                            path_params : {
+                                list_id : i
                                 }
-                            }, function (err, result) {
-                                if (err) {
-                                    reject(err);
-                                }
+                            });
+
+/*                        act_req.push({
+                            method: 'get',
+                            path: 'lists/{list_id}/activity',
+                            path_params : {
+                                list_id : i
+                            }
+                        });*/
+                    }
+
+                    for (var j = 0; j < result.reports.length; j++) {
+                        hist_req.push({
+                            method: 'get',
+                            path: '/reports/{report_id}/',
+                            path_params : {
+                                report_id : result.reports[j].id
+                            }
+                        });
+                    }
+
+                    var get_history = new Promise(function(resolve, reject) {
+                        var data = mailchimp.batch(hist_req, function (err, result) {
+                            if (err) {
+                                reject(err);
+                            }
+                            resolve(result);
+                        })
+                    })
+
+                    get_history.then(function(result) {
+                        var top = [];
+                        var down = [];
+                        var campaign = [];
+                        result.forEach(function (row, i) {
+                            if (("history" in row)) {                                
                                 var subs = 0; //Variable for total Subscriptions
                                 var monthly = [];
-
-                                //Loop through each month
-                                result.history.forEach(function (row, i) {
+                                row.history.forEach(function (row, j) {
                                     subs = subs + row.imports + row.optins;
                                     monthly.push([row.month, row.imports + row.optins]);
                                 });
-
-                                resolve({
-                                    top: {
-                                    name: title,
-                                    y: subs,
-                                    drilldown: title
-                                    },
-                                    down: {
-                                    id: title,
-                                    name: title,
+                                top.push({
+                                        name: map[row.list_id],
+                                        y: subs,
+                                        drilldown: map[row.list_id]
+                                    })
+                                down.push({
+                                    id: map[row.list_id],
+                                    name: map[row.list_id],
                                     data: monthly
-                                    }
-                                });
-                            })
-                        })
-
-                        get_history.then(function(result) {
-                            counter = counter+1;
-                            TopLevel.push(result.top);
-                            DetailedData.push(result.down);
-                            if (counter == limit) {
-                                res.send({ obj1: TopLevel, obj2: DetailedData});
+                                })
                             }
-                        }, function(err) {
-                            console.log(err);
+                            else if ("campaign_title" in row) {
+                                var open_rate = 0;
+                                var click_rate = 0;
+
+                                campaign.push({
+                                    title : row.campaign_title,
+                                    open_rate : row.opens.open_rate * 100,
+                                    click_rate : row.clicks.click_rate * 100
+                                });
+                                
+                            }
                         });
-                    }
+                        MCdata.push({ obj1: top});
+                        MCdata.push({ obj2: down});
+                        MCdata.push({ obj3: campaign});
+
+                        res.send(MCdata);
+                    }, function(err) {
+                        console.log(err);
+                    });
+
+/*                    var get_activity = new Promise(function(resolve, reject) {
+                        var data = mailchimp.batch(act_req, function (err, result) {
+                            if (err) {
+                                reject(err);
+                            }
+                            resolve(result)
+                        })
+                    })
+
+                    get_activity.then(function(result) {
+                        res.send(result);
+                    }, function(err) {
+                        console.log(err);
+                    });
+*/
+
+
                 }, function(err) {
                     console.log(err);
                 });
