dd0af358372b2d1b18a99aa1cc9728f1cc66aadf
mishran1 <nishantmishra0@gmail.com>
2016-08-08 16:24:17 -0400

    Fixed dashboard graph containers

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -3,57 +3,210 @@
 
     angular
         .module('app')
-        .controller('Home.IndexController', ['$scope', '$timeout',
-            function ($scope, $timeout) { 
-                $scope.tester = 'RIP Harambe!';
+        .controller('Home.IndexController', ['$scope', '$timeout', 'UserService',
+            function ($scope, $timeout, UserService, $watch) {
+                let dataMC;
+                var names = [];
+                var click_rates = [];
+                var open_rates = [];
+
+                // Set the theme
+                Highcharts.theme = {
+                    colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', 
+                             '#FF9655', '#FFF263', '#6AF9C4'],
+                    chart: {
+                        backgroundColor: {
+                            linearGradient: [0, 0, 500, 500],
+                            stops: [
+                                [0, 'rgb(255, 255, 255)'],
+                                [1, 'rgb(0, 4, 255)']
+                            ]
+                        },
+                    },
+                    title: {
+                        style: {
+                            color: '#000',
+                            font: 'bold 16px "Avenir", Verdana, sans-serif'
+                        }
+                    },
+                    subtitle: {
+                        style: {
+                            color: '#666666',
+                            font: 'bold 12px "Avenir", Verdana, sans-serif'
+                        }
+                    },
+
+                    legend: {
+                        itemStyle: {
+                            font: '9pt Avenir, Verdana, sans-serif',
+                            color: 'black'
+                        },
+                        itemHoverStyle:{
+                            color: 'black'
+                        }   
+                    }
+                };
+
+                // Apply the theme
+                Highcharts.setOptions(Highcharts.theme);
+
+                UserService.GetCurrent().then(function (user) {
+                    dataMC = user.dataMC;
+                })
+                .then(function() {
+                    setTimeout(function() { 
+                        chart0();
+                        chart1();
+                    }, 500);
+
+                    dataMC[2].obj3.forEach(function(row, i) {
+                      click_rates.push(row.click_rate);
+                      open_rates.push(row.open_rate);
+                      names.push(row.title);
+                    });
+                });
+
                 $scope.gridsterOptions = {
                     margins: [20, 20],
                     columns: 4,
                     draggable: {
                         handle: 'h3'
-                    }
+                    },
+                    rowHeight: 'match'
                 };
 
-                $scope.dashboards = {
-                    '1': {
-                        id: '1',
+                $scope.dashboard = {
                         name: 'Home',
                         widgets: [{
                             col: 0,
                             row: 0,
-                            sizeY: 1,
-                            sizeX: 1,
+                            sizeY: 2,
+                            sizeX: 2,
+                            index: 0,
                             name: "Widget 1"
                         }, {
                             col: 2,
-                            row: 1,
-                            sizeY: 1,
-                            sizeX: 1,
+                            row: 0,
+                            sizeY: 2,
+                            sizeX: 2,
+                            index: 1,
                             name: "Widget 2"
                         }]
-                    },
-                    '2': {
-                        id: '2',
-                        name: 'Other',
-                        widgets: [{
-                            col: 1,
-                            row: 1,
-                            sizeY: 1,
-                            sizeX: 2,
-                            name: "Other Widget 1"
-                        }, {
-                            col: 1,
-                            row: 3,
-                            sizeY: 1,
-                            sizeX: 1,
-                            name: "Other Widget 2"
-                        }]
-                    }
                 };
 
                 $scope.clear = function() {
                     $scope.dashboard.widgets = [];
                 };
+
+                var chart0 = function () {
+                  Highcharts.chart('mc-container-0', {
+                    chart: {
+                      type: 'column'
+                    },
+
+                    title: {
+                        text: 'Mailing List Subscribers'
+                    },
+
+                    subtitle: {
+                        text: 'Click the columns to view monthly subscribers'
+                    },
+
+                    xAxis: {
+                        type: 'category'
+                    },
+
+                    yAxis: {
+                        title: {
+                            text: 'Users'
+                        }
+
+                    },
+
+                    credits: {
+                      enabled: false
+                    },
+
+                    legend: {
+                        enabled: false
+                    },
+
+                    plotOptions: {
+                        series: {
+                            borderWidth: 0,
+                            dataLabels: {
+                                enabled: true,
+                                format: '{point.y}'
+                            }
+                        }
+                    },
+
+                    tooltip: {
+                        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> users added to Total Subscribers<br/>'
+                    },
+
+                    series: [{
+                        name: 'Lists',
+                        colorByPoint: true,
+                        data: dataMC[0].obj1
+                    }],
+
+                    drilldown: {
+                        series: dataMC[1].obj2
+                    }
+                  })
+                };
+
+                var chart1 = function () {
+                  Highcharts.chart('mc-container-1', {
+                    title: {
+                      text: 'Engagement Analysis'
+                    },
+                    xAxis: {
+                      categories: names
+                    },
+                    labels: {
+                        items: [{
+                            html: 'Total fruit consumption',
+                            style: {
+                                left: '50px',
+                                top: '18px',
+                                color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+                            }
+                        }]
+                    },
+                    credits: {
+                      enabled: false
+                    },
+                    series: [{
+                        type: 'column',
+                        name: 'Click Rate',
+                        data: click_rates
+                    }, {
+                        type: 'column',
+                        name: 'Open Rate',
+                        data: open_rates
+                    }]
+                  });
+                }
+
+
+                $scope.$watch('[dashboard.widgets[0].sizeX, [dashboard.widgets[0].sizeY]]', function(newX, oldX) {
+                  if (newX !== oldX) {
+                    setTimeout(function() { 
+                        chart0();
+                    }, 2000);
+                  }
+                });
+
+                $scope.$watch('[dashboard.widgets[1].sizeX, [dashboard.widgets[1].sizeY]]', function(newX, oldX) {
+                  if (newX !== oldX) {
+                    setTimeout(function() { 
+                        chart1();
+                    }, 2000);
+                  }
+                });
 
                 $scope.addWidget = function() {
                     $scope.dashboard.widgets.push({
@@ -62,18 +215,6 @@
                         sizeY: 1
                     });
                 };
-
-                $scope.$watch('selectedDashboardId', function(newVal, oldVal) {
-                    if (newVal !== oldVal) {
-                        $scope.dashboard = $scope.dashboards[newVal];
-                    } else {
-                        $scope.dashboard = $scope.dashboards[1];
-                    }
-                });
-
-                // init dashboard
-                $scope.selectedDashboardId = '1';
-
             }
         ])
 
@@ -141,4 +282,8 @@
 
             }
         ]);
+
+        (function () {
+
+        })
 })();
